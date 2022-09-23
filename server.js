100dbf0e5685a6dbe83a8dff605049e8251fc4f2
Artur Kovacs <artur.kovacs@codecool.com>
2022-09-23 08:59:08 +0200

    solve first two tasks

--- server.js
+++ server.js
@@ -2,28 +2,37 @@ const http = require('http');
 const fs = require('fs');
 
 const ip = "127.0.0.1";
-const port = 9000;
+const port = 9001;
 const server = http.createServer((request, response) => {
-
 	//request parameter contains the incoming (request) data from the client side to the server side as an object 
-	console.log(request);
 	console.log(request.method);
 	console.log(request.url);
 
-	
-	//response parameter contains the outgoing (response) data from the server side to the client side as an object 
-	console.log(response);
-	
-	
-	//Write your code here
-	//you can read the file with the help of the fs package's readFileSync method and parse the JSON with JSON.parse method
+	const pkgs = readPackages();
+	// console.log(JSON.stringify(pkgs));
+	if (request.url === "/") {
+		let htmlContents = fs.readFileSync("./index.html").toString();
+		htmlContents = htmlContents.replace(
+			"LATEST_NPM_RELEASE_DATE",
+			pkgs[0].releases[0].date
+		);
+		response.write(htmlContents);
+	} else if (request.url == "/api/package/") {
+		console.log("responding to api/package")
+		response.setHeader("Content-Type", "application/json");
+		response.write(JSON.stringify(pkgs));
+	}
 
 	//Don't forget to close your response
 	response.end();
-
 });
-
 server.listen(port, ip, () => {
 	const addr = server.address();
 	console.log(`http://${addr.address}:${addr.port}`);
 });
+
+function readPackages() {
+	const jsonString = fs.readFileSync("./pkgs.json").toString();
+	const pkgs = JSON.parse(jsonString);
+	return pkgs.packages;
+}
